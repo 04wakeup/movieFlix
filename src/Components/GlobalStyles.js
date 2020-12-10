d20f7dc97083d06c4fe93fe4adb53ac691fc86d0
04wakeup <42279557+04wakeup@users.noreply.github.com>
2020-12-09 20:00:39 -0800

    add style on header

--- src/Components/GlobalStyles.js
+++ src/Components/GlobalStyles.js
@@ -13,8 +13,10 @@ const globalStyles = createGlobalStyle`
     }
     body{
         font-family: -apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Arial,Noto Sans,sans-serif;
-        font-size: 14px;
+        font-size: 12px;
         background-color: rgba(20,20,20,1);
+        color: white;
+        padding-top: 50px;
     }
 `;
 
