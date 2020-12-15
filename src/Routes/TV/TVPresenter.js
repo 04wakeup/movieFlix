333bfcf4ada5a1a79f5e5a71a44f48cecaf7af91
04wakeup <42279557+04wakeup@users.noreply.github.com>
2020-12-15 11:41:21 -0800

    update Home & TV

--- src/Routes/TV/TVPresenter.js
+++ src/Routes/TV/TVPresenter.js
@@ -1,10 +1,30 @@
+/* eslint-disable max-len */
+/* eslint-disable react/require-default-props */
 /* eslint-disable react/forbid-prop-types */
 /* eslint-disable no-unused-vars */
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
+// eslint-disable-next-line import/no-unresolved
+import Section from "Components/Section";
+// eslint-disable-next-line import/no-unresolved
+import Loader from "Components/Loader";
 
-const TVPresenter = ({ topRated, popular, airingToday, loading, error }) => null;
+const Container = styled.div`
+  font-size: 10px;
+`;
+
+const TVPresenter = ({ topRated, popular, airingToday, loading, error }) =>
+  // eslint-disable-next-line max-len
+  loading ? (
+    <Loader />
+  ) : (
+    <Container>
+      {topRated && topRated.length > 0 && <Section title="Top Rated Shows">{topRated.map((show) => show.name)}</Section>}
+      {popular && popular.length > 0 && <Section title="Popular Shows">{popular.map((show) => show.name)}</Section>}
+      {airingToday && airingToday.length > 0 && <Section title="Airing Shows">{airingToday.map((show) => show.name)}</Section>}
+    </Container>
+  );
 
 // Constraint Check
 TVPresenter.propTypes = {
